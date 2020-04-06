f5feeb78801ee69a3f55a7efcb77100d16316025
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2020-04-07 01:06:32 +0200

    Fix script again

--- scripts/test_case_1.sh
+++ scripts/test_case_1.sh
@@ -10,21 +10,7 @@ start: {x: -15.97, y: -11.0, theta: 1.603}
 goal: {x: -15.343, y: 11.00, theta: 1.586}
 mod_type: 0
 upstream: false
-weight_c: 0.01"
-
-sleep 1
-
-rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:                                                                                              
-  seq: 0
-  stamp: {secs: 1352597736, nsecs: 0}
-  frame_id: 'map'
-times: 5
-planning_time_limit: $1
-start: {x: -15.97, y: -11.0, theta: 1.603}
-goal: {x: -15.343, y: 11.00, theta: 1.586}
-mod_type: 0
-upstream: false
-weight_c: 0.01"
+weight_c: 0.001"
 
 sleep 10
 
@@ -38,7 +24,7 @@ start: {x: -15.97, y: -11.0, theta: 1.603}
 goal: {x: -15.343, y: 11.00, theta: 1.586}
 mod_type: 3
 upstream: false
-weight_c: 0.01"
+weight_c: 0.001"
 
 sleep 10
 
@@ -52,7 +38,7 @@ start: {x: -15.97, y: -11.0, theta: 1.603}
 goal: {x: -15.343, y: 11.00, theta: 1.586}
 mod_type: 0
 upstream: true
-weight_c: 0.01"
+weight_c: 0.1"
 
 sleep 10
 
@@ -66,7 +52,7 @@ start: {x: -15.97, y: -11.0, theta: 1.603}
 goal: {x: -15.343, y: 11.00, theta: 1.586}
 mod_type: 1
 upstream: true
-weight_c: 0.01"
+weight_c: 0.001"
 
 sleep 10
 
@@ -80,4 +66,4 @@ start: {x: -15.97, y: -11.0, theta: 1.603}
 goal: {x: -15.343, y: 11.00, theta: 1.586}
 mod_type: 2
 upstream: true
-weight_c: 0.01"
+weight_c: 0.1"
