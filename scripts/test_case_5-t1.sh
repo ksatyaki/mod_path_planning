8c3808d4da9024b0b3c384d5f2db3d1d4220b067
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2021-03-23 11:41:30 +0530

    Change some start-goal pairs and add one more

--- scripts/test_case_5-t1.sh
+++ scripts/test_case_5-t1.sh
@@ -11,6 +11,20 @@ times: $2
 planning_time_limit: $1
 start: {x: -0.133, y: -8.555, theta: 2.604}
 goal: {x: 31.093, y: -20.553, theta: -1.999}
+mod_type: 2
+upstream: true
+weight_c: 0.1"
+
+sleep 10
+
+rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
+  seq: 0
+  stamp: {secs: 1352866100, nsecs: 0}
+  frame_id: 'map'
+times: $2
+planning_time_limit: $1
+start: {x: -0.133, y: -8.555, theta: 2.604}
+goal: {x: 31.093, y: -20.553, theta: -1.999}
 mod_type: 0
 upstream: false
 weight_c: 0.00"
@@ -21,50 +35,36 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-goal: {x: -3.85, y: -6.82, theta: 2.337}
-start: {x: 21.471, y: -17.647, theta: -2.411}
+start: {x: -0.133, y: -8.555, theta: 2.604}
+goal: {x: 31.093, y: -20.553, theta: -1.999}
 mod_type: 0
 upstream: false
 weight_c: 0.02"
 
 sleep 10
 
-rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:                                                                                              
+rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   seq: 0
   stamp: {secs: 1352866100, nsecs: 0}
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-goal: {x: -3.85, y: -6.82, theta: 2.337}
-start: {x: 21.471, y: -17.647, theta: -2.411}
+start: {x: -0.133, y: -8.555, theta: 2.604}
+goal: {x: 31.093, y: -20.553, theta: -1.999}
 mod_type: 0
 upstream: true
 weight_c: 0.1"
 
 sleep 10
 
-rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:                                                                                              
+rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   seq: 0
   stamp: {secs: 1352866100, nsecs: 0}
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-goal: {x: -3.85, y: -6.82, theta: 2.337}
-start: {x: 21.471, y: -17.647, theta: -2.411}
+start: {x: -0.133, y: -8.555, theta: 2.604}
+goal: {x: 31.093, y: -20.553, theta: -1.999}
 mod_type: 1
-upstream: true
-weight_c: 0.1"
-
-sleep 10
-
-rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:                                                                                              
-  seq: 0
-  stamp: {secs: 1352866100, nsecs: 0}
-  frame_id: 'map'
-times: $2
-planning_time_limit: $1
-goal: {x: -3.85, y: -6.82, theta: 2.337}
-start: {x: 21.471, y: -17.647, theta: -2.411}
-mod_type: 2
 upstream: true
 weight_c: 0.1"
--- scripts/test_case_5-t2.sh
+++ scripts/test_case_5-t2.sh
@@ -8,6 +8,20 @@ times: $2
 planning_time_limit: $1
 start: {x: -0.133, y: -8.555, theta: 2.604}
 goal: {x: 31.093, y: -20.553, theta: -1.999}
+mod_type: 2
+upstream: true
+weight_c: 0.1"
+
+sleep 10
+
+rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
+  seq: 0
+  stamp: {secs: 1352883600, nsecs: 0}
+  frame_id: 'map'
+times: $2
+planning_time_limit: $1
+start: {x: -0.133, y: -8.555, theta: 2.604}
+goal: {x: 31.093, y: -20.553, theta: -1.999}
 mod_type: 0
 upstream: false
 weight_c: 0.00"
@@ -49,19 +63,5 @@ planning_time_limit: $1
 start: {x: -0.133, y: -8.555, theta: 2.604}
 goal: {x: 31.093, y: -20.553, theta: -1.999}
 mod_type: 1
-upstream: true
-weight_c: 0.1"
-
-sleep 10
-
-rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:                                                                                              
-  seq: 0
-  stamp: {secs: 1352883600, nsecs: 0}
-  frame_id: 'map'
-times: $2
-planning_time_limit: $1
-start: {x: -0.133, y: -8.555, theta: 2.604}
-goal: {x: 31.093, y: -20.553, theta: -1.999}
-mod_type: 2
 upstream: true
 weight_c: 0.1"
