70e033b8eb72f467a6e5fd7e25a11fff00b2f249
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2021-08-27 19:20:35 +0200

    WIP

--- src/compute_costs_from_path.cpp
+++ src/compute_costs_from_path.cpp
@@ -1,16 +1,23 @@
 #include <ompl/mod/objectives/DTCOptimizationObjective.h>
 #include <ompl/mod/objectives/IntensityMapOptimizationObjective.h>
 #include <ompl/mod/objectives/UpstreamCriterionOptimizationObjective.h>
+#include <ros/console.h>
+#include <ros/ros.h>
 
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <boost/regex.hpp>
+#include <cliffmap_ros/cliffmap.hpp>
+#include <gmmtmap_ros/gmmtmap.hpp>
+#include <ompl_planners_ros/car_state_space.hpp>
+#include <stefmap_ros/stefmap.hpp>
 #include <string>
 
 namespace po = boost::program_options;
+namespace ob = ompl::base;
 
 std::vector<std::string> getFilesMatchingPattern(
-    const std::string& target_path, const std::string& pattern = "*.path") {
+    const std::string &target_path, const std::string &pattern = "*.path") {
   // const boost::regex my_filter(pattern);
 
   std::vector<std::string> all_matching_files;
@@ -28,7 +35,32 @@ std::vector<std::string> getFilesMatchingPattern(
   return all_matching_files;
 }
 
-int main(int argn, char* args[]) {
+void computeCostsFromCSV(const std::string &csv_file_name,
+                         const ompl::mod::MoDOptimizationObjectivePtr &ptr,
+                         const std::string &type) {
+  std::string costs_file_name = csv_file_name;
+  boost::regex_replace(costs_file_name, boost::regex(".path"), ".costs_file");
+
+  std::fstream path_file(csv_file_name, std::ios::in);
+  std::fstream costs_file(costs_file_name, std::ios::out);
+
+  if (!path_file) {
+    ROS_INFO_STREAM("Somehow couldn't open this file for reading "
+                    << csv_file_name);
+  }
+  if (!costs_file) {
+    ROS_INFO_STREAM("Somehow couldn't open this file for writing "
+                    << costs_file_name);
+  }
+
+  ompl::mod::MoDOptimizationObjectivePtr concOptObj;
+
+  std::vector<std::array<double, 3>> poses;
+}
+
+int main(int argn, char *args[]) {
+  ros::init(argn, args, "paths_to_costs");
+
   po::options_description desc("allowed options");
   desc.add_options()("help", "produce help message")(
       "folder", po::value<std::string>(), "Folder where the paths are stored.");
@@ -52,15 +84,52 @@ int main(int argn, char* args[]) {
   }
 
   std::string folder_path = vm["folder"].as<std::string>();
-  char* scenarios[] = {"corridor1", "corridor2", "corridor3", "corridor4"};
-  char* times[] = {"t1", "t2", "t3"};
+  const std::string scenarios[4] = {"corridor1", "corridor2", "corridor3",
+                                    "corridor4"};
+  const std::string times[3] = {"t1", "t2", "t3"};
 
   std::vector<std::string> all_path_files;
 
-  for (const std::string& scenario : scenarios) {
-    for (const std::string& time : times) {
+  for (const std::string &scenario : scenarios) {
+    for (const std::string &time : times) {
+      std::string this_folder = folder_path + "/" + scenario + "/" + time;
+      auto this_files = getFilesMatchingPattern(this_folder);
+      all_path_files.insert(all_path_files.end(), this_files.begin(),
+                            this_files.end());
     }
   }
+
+  std::shared_ptr<ob::CarStateSpace> cStateSpace =
+      std::make_shared<ob::CarStateSpace>(0.5);
+  std::shared_ptr<ob::SpaceInformation> spaceInfo =
+      std::make_shared<ob::SpaceInformation>(cStateSpace);
+
+  auto cliffmap_client = std::make_shared<cliffmap_ros::CLiFFMapClient>();
+  auto stefmap_client = std::make_shared<stefmap_ros::STeFMapClient>();
+  auto gmmtmap_client = std::make_shared<gmmtmap_ros::GMMTMapClient>();
+
+  double planning_time = 0.0;
+
+  constexpr double weight_d = 1.0, weight_q = 1.0, weight_c = 0.1;
+
+  ompl::mod::DTCOptimizationObjective DTCCostObjective(
+      spaceInfo, cliffmap_client->get(), weight_d, weight_q, 0.02, 1.0);
+
+  ompl::mod::UpstreamCriterionOptimizationObjective STeFUpstreamCostObjective(
+      spaceInfo, stefmap_client->get(planning_time, 2), weight_c, weight_q,
+      weight_c);
+
+  ompl::mod::UpstreamCriterionOptimizationObjective GMMTUpstreamCostObjective(
+      spaceInfo, gmmtmap_client->get(), weight_d, weight_q, weight_c);
+
+  ompl::mod::UpstreamCriterionOptimizationObjective CLiFFUpstreamCostObjective(
+      spaceInfo, cliffmap_client->get(), weight_d, weight_q, weight_c);
+
+  ompl::mod::IntensityMapOptimizationObjective IntensityCostObjective(
+      spaceInfo, "/home/ksatyaki/intensity_map_1m.xml", weight_d, weight_q,
+      weight_c * 2);
+
+  ROS_INFO_STREAM("All MoD Optimization Objectives initialized.");
 
   return 0;
 }
