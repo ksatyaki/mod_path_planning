664eab6715ef747ed85e7fabca8bded7f8bd3bd3
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2021-10-01 17:50:48 +0200

    Pedsim experiments

--- scripts/toy_examples/toy_example_1.sh
+++ scripts/toy_examples/toy_example_1.sh
@@ -1,7 +1,14 @@
 #!/bin/zsh
 
-# Setting goal: Frame:map, Position(5,731, 4,705, 0,000), Orientation(0,000, 0,000, 0,728, 0,685) = Angle: 1,632
-# Setting goal: Frame:map, Position(4,876, 22,589, 0,000), Orientation(0,000, 0,000, 0,730, 0,684) = Angle: 1,635
+# x: 0.08393287658691406
+# y: 27.059646606445312
+#    z: 0.028810509310999877
+#    w: 0.9995848911189288
+
+# x: 30.613258361816406
+# y: 0.7859249114990234
+#     z: -0.7123635930770941
+#     w: 0.7018105950028771
 
 rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   seq: 0
@@ -9,8 +16,8 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-start: {x: 5.731, y: 4.705, theta: 1.632}
-goal: {x: 4.876, y: 22.589, theta: 1.632}
+start: {x: 0.0, y: 27.0, theta: 0.0}
+goal: {x: 30.0, y: 0.0, theta: -1.57079632679}
 mod_type: 0
 upstream: false
 weight_c: 0.00"
@@ -21,13 +28,11 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-start: {x: 5.731, y: 4.705, theta: 1.632}
-goal: {x: 4.876, y: 22.589, theta: 1.632}
+start: {x: 0.0, y: 27.0, theta: 0.0}
+goal: {x: 30.0, y: 0.0, theta: -1.57079632679}
 mod_type: 0
 upstream: false
 weight_c: 0.02"
-
-sleep 10
 
 rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:                                                                                              
   seq: 0
@@ -35,13 +40,11 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-start: {x: 5.731, y: 4.705, theta: 1.632}
-goal: {x: 4.876, y: 22.589, theta: 1.632}
+start: {x: 0.0, y: 27.0, theta: 0.0}
+goal: {x: 30.0, y: 0.0, theta: -1.57079632679}
 mod_type: 0
 upstream: true
 weight_c: 0.1"
-
-sleep 10
 
 rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   seq: 0
@@ -49,8 +52,20 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-start: {x: 5.731, y: 4.705, theta: 1.632}
-goal: {x: 4.876, y: 22.589, theta: 1.632}
+start: {x: 0.0, y: 27.0, theta: 0.0}
+goal: {x: 30.0, y: 0.0, theta: -1.57079632679}
 mod_type: 2
 upstream: true
 weight_c: 0.1"
+
+rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
+  seq: 0
+  stamp: {secs: 1352866100, nsecs: 0}
+  frame_id: 'map'
+times: $2
+planning_time_limit: $1
+start: {x: 0.0, y: 27.0, theta: 0.0}
+goal: {x: 30.0, y: 0.0, theta: -1.57079632679}
+mod_type: 4
+upstream: true
+weight_c: 0.2"
--- scripts/toy_examples/toy_example_2.sh
+++ scripts/toy_examples/toy_example_2.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
+# x: 30.243206024169922
+# y: -0.4475841522216797
+#    z: 0.7291656139070131
+#    w: 0.6843372761260406
 
-# Setting goal: Frame:map, Position(29,369, 10,420, 0,000), Orientation(0,000, 0,000, 0,729, 0,685) = Angle: 1,633
-# Setting goal: Frame:map, Position(29,097, 22,473, 0,000), Orientation(0,000, 0,000, 0,727, 0,687) = Angle: 1,628
-
-
-
+# x: 2.2425737380981445
+# y: 27.614727020263672
+#    z: 0.9993821931641956
+#    w: 0.03514586727230288
 
 rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   seq: 0
@@ -12,8 +15,8 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-start: {x: 29.369, y: 10.420, theta: 1.632}
-goal: {x: 29.369, y: 22.589, theta: 1.632}
+goal: {x: 0.0, y: 27.0, theta: 0.0}
+start: {x: 30.0, y: 0.0, theta: 1.57079632679}
 mod_type: 0
 upstream: false
 weight_c: 0.00"
@@ -24,13 +27,11 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-start: {x: 29.369, y: 10.420, theta: 1.632}
-goal: {x: 29.369, y: 22.589, theta: 1.632}
+goal: {x: 0.0, y: 27.0, theta: 0.0}
+start: {x: 30.0, y: 0.0, theta: 1.57079632679}
 mod_type: 0
 upstream: false
 weight_c: 0.02"
-
-sleep 10
 
 rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:                                                                                              
   seq: 0
@@ -38,13 +39,11 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-start: {x: 29.369, y: 10.420, theta: 1.632}
-goal: {x: 29.369, y: 22.589, theta: 1.632}
+goal: {x: 0.0, y: 27.0, theta: 0.0}
+start: {x: 30.0, y: 0.0, theta: 1.57079632679}
 mod_type: 0
 upstream: true
 weight_c: 0.1"
-
-sleep 10
 
 rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   seq: 0
@@ -52,8 +51,20 @@ rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
   frame_id: 'map'
 times: $2
 planning_time_limit: $1
-start: {x: 29.369, y: 10.420, theta: 1.632}
-goal: {x: 29.369, y: 22.589, theta: 1.632}
+goal: {x: 0.0, y: 27.0, theta: 0.0}
+start: {x: 30.0, y: 0.0, theta: 1.57079632679}
 mod_type: 2
 upstream: true
 weight_c: 0.1"
+
+rostopic pub -1 /mod_planning_goal mod_path_planning/MoDPlanningGoal "header:
+  seq: 0
+  stamp: {secs: 1352866100, nsecs: 0}
+  frame_id: 'map'
+times: $2
+planning_time_limit: $1
+goal: {x: 0.0, y: 27.0, theta: 0.0}
+start: {x: 30.0, y: 0.0, theta: 1.57079632679}
+mod_type: 4
+upstream: true
+weight_c: 0.2"
